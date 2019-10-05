f9a69f72d0e3feb863321c25caee57f0e05e149c
kboul <kos.voulgaridis@gmail.com>
2019-10-05 20:07:21 +0300

    Refactor Questions & add prop-types

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getQuestions } from '../store/actions';
 
 class Questions extends Component {
     componentDidMount() {
-        this.props.getQuestions();
+        const { getQuestions } = this.props;
+        getQuestions();
     }
 
     render() {
-        console.log(this.props);
+        const { questions } = this.props;
+        console.log(questions);
         return <div>Questions component</div>;
     }
 }
 
 const mapStateToProps = state => ({
-    questions: state.questions
+    questions: state.questions.questions
 });
 
 const mapDispatchToProps = {
     getQuestions
+};
+
+Questions.propTypes = {
+    questions: PropTypes.arrayOf(
+        PropTypes.shape({
+            answers: PropTypes.arrayOf(
+                PropTypes.shape({
+                    order: PropTypes.number.isRequired,
+                    body: PropTypes.string.isRequired
+                })
+            ),
+            id: PropTypes.string.isRequired,
+            order: PropTypes.number.isRequired,
+            prompt: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    getQuestions: PropTypes.func.isRequired
 };
 
 export default connect(
