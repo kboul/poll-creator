1b45be41d7c6e36f9fb7a8636b7d74719d6c2c19
kboul <kos.voulgaridis@gmail.com>
2019-10-05 18:40:19 +0300

    Fix questions endpoint on getQuestions action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,7 +4,7 @@ import { types } from './types';
 export const getQuestions = () => {
     return async dispatch => {
         try {
-            const response = await httpService.get('/api/questions/1');
+            const response = await httpService.get('/api/questions');
             dispatch({
                 type: types.GET_QUESTIONS,
                 questions: response.data
