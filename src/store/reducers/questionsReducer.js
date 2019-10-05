81d8f8209ab9151778b8d8a1da5726f4c511733c
kboul <kos.voulgaridis@gmail.com>
2019-10-05 18:39:00 +0300

    Create questionsReducer & handle get questions case

--- src/store/reducers/questionsReducer.js
+++ src/store/reducers/questionsReducer.js
@@ -1,5 +1,13 @@
+import { types } from '../actions/types';
+
 const questionsReducer = (state = [], action) => {
     switch (action.type) {
+        case types.GET_QUESTIONS:
+            const questions = action.questions;
+            return {
+                ...state,
+                questions
+            };
         default:
             return state;
     }
