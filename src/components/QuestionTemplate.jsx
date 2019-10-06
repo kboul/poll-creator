af854cc6bde9f999f9223cd9cd7740dbcf2d312a
kboul <kos.voulgaridis@gmail.com>
2019-10-06 18:50:10 +0300

    Add my-4 bootstrap class on QuestionTemplate

--- src/components/QuestionTemplate.jsx
+++ src/components/QuestionTemplate.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const QuestionTemplate = () => {
     return (
-        <div className="card">
+        <div className="card my-4">
             <div className="card-body">
                 This is some text within a card body.
             </div>
