66975459d81f49f4651f109554050943667fbdc3
kboul <kos.voulgaridis@gmail.com>
2019-10-06 18:49:36 +0300

    Extract AddNewQuestionBtn to a separate component

---
+++ src/components/AddNewQuestionBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddNewQuestionBtn = () => {
+    return (
+        <div className="offset-md-5">
+            <button type="button" className="btn btn-primary">
+                Add new question
+            </button>
+        </div>
+    );
+};
+
+export default AddNewQuestionBtn;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Questions from './Questions';
 import QuestionTemplate from './QuestionTemplate';
+import AddNewQuestionBtn from './AddNewQuestionBtn';
 
 const App = () => {
     return (
-        <div className="container-fluid mt-4">
+        <div className="container-fluid">
             <div className="col-md-6 offset-md-3">
                 <QuestionTemplate />
+                <AddNewQuestionBtn />
                 <Questions />
             </div>
         </div>
