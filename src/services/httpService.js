0a65ad8d2be0d3fdc87b3efaea8e09aa79151084
kboul <kos.voulgaridis@gmail.com>
2019-10-06 19:36:56 +0300

    Log errors in httpService

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,6 +3,17 @@ import axios from 'axios';
 // define app API url
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
+axios.interceptors.response.use(null, error => {
+    const expectedError =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500;
+    if (!expectedError) {
+        console.log('Logging the error ', error);
+    }
+    return Promise.reject(error);
+});
+
 export default {
     get: axios.get,
     post: axios.post,
