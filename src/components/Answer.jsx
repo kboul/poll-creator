c0882d41d2c5c353434ca3ac521ecdad028ab2ad
kboul <kos.voulgaridis@gmail.com>
2019-10-07 19:39:55 +0300

    Implement updateAnswer functionality

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Icons from './Icons';
+import SaveButton from './SaveButton';
+import { updateAnswer } from '../store/actions/updateAnswer';
 import { deleteAnswer } from '../store/actions/deleteAnswer';
 import styles from '../sass/Answer.module.sass';
 
-const Answer = ({ id, body, order, deleteAnswer }) => {
+const Answer = ({ id, body, order, updateAnswer, deleteAnswer }) => {
+    const [value, setValue] = useState(body);
+    const [toggleSave, setToggleSave] = useState(false);
+
     return (
         <div className="row no-gutters">
             <div className="col-xl-10 col-lg-10 col-md-9 col-sm-10 my-auto">
@@ -19,9 +24,20 @@ const Answer = ({ id, body, order, deleteAnswer }) => {
                     <input
                         type="text"
                         className={`form-control ${styles.input}`}
-                        value={body}
-                        onChange={e => console.log(e)}
+                        value={value}
+                        onChange={e => {
+                            setValue(e.target.value);
+                            setToggleSave(true);
+                        }}
                     />
+                    {toggleSave && (
+                        <SaveButton
+                            onClick={() => {
+                                updateAnswer(value, id, order);
+                                setToggleSave(false);
+                            }}
+                        />
+                    )}
                 </div>
             </div>
             <div className="col-xl-2 col-lg-2 col-md-3 col-sm-2 my-auto">
@@ -35,10 +51,12 @@ Answer.propTypes = {
     id: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
     order: PropTypes.number.isRequired,
+    updateAnswer: PropTypes.func.isRequired,
     deleteAnswer: PropTypes.func.isRequired
 };
 
 const mapDispatchToProps = {
+    updateAnswer,
     deleteAnswer
 };
 
