b9c53721fa7954250ba46cd139ce755582637cc1
kboul <kos.voulgaridis@gmail.com>
2019-10-07 08:33:07 +0300

    Dispatch GET_QUESTIONS_LOADING & GET_QUESTIONS_FAIL actions on getQuestions action to toggle a Spinner

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,16 +4,18 @@ import { types } from './types';
 export const getQuestions = () => {
     return async dispatch => {
         try {
+            dispatch({ type: types.GET_QUESTIONS_LOADING });
             const response = await httpService.get('/api/questions');
             dispatch({
                 type: types.GET_QUESTIONS,
                 questions: response.data.data
             });
         } catch (error) {
-            console.error(
+            console.log(
                 'There was an error while fetching the questions',
                 error
             );
+            dispatch({ type: types.GET_QUESTIONS_FAIL });
         }
     };
 };
