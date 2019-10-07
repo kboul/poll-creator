09913f474a84369b1bbb15d39caa4dbb6ab1d661
kboul <kos.voulgaridis@gmail.com>
2019-10-07 08:21:07 +0300

    Create Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className="mt-2 text-center">
+            <i className="fa fa-spin fa-spinner fa-2x" />
+        </div>
+    );
+};
+
+export default Spinner;
