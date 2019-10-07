7a6cc408ccdbe613c967d3ea907ae004f9b27c16
kboul <kos.voulgaridis@gmail.com>
2019-10-07 08:48:03 +0300

    Display a Spinner while fetching questions, show an error message if request failed, show a message if questions have not been created yet on Questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from './Spinner';
 import { getQuestions } from '../store/actions';
 
 class Questions extends Component {
@@ -10,15 +11,32 @@ class Questions extends Component {
     }
 
     render() {
-        const { questions } = this.props;
+        const {
+            questions: { questions, loading, error }
+        } = this.props;
         console.log(questions);
 
-        return <div>Questions component</div>;
+        if (error && !questions.length)
+            return (
+                <div className="alert alert-danger mt-2">
+                    There was an error while fetching the questions.
+                </div>
+            );
+
+        const questionnaire = !questions.length ? (
+            <div className="alert alert-warning mt-2">
+                No questions have been created yet.
+            </div>
+        ) : (
+            <div>Questions component</div>
+        );
+
+        return loading ? <Spinner /> : questionnaire;
     }
 }
 
 const mapStateToProps = state => ({
-    questions: state.questions.questions
+    questions: state.questions
 });
 
 const mapDispatchToProps = {
@@ -26,19 +44,23 @@ const mapDispatchToProps = {
 };
 
 Questions.propTypes = {
-    questions: PropTypes.arrayOf(
-        PropTypes.shape({
-            answers: PropTypes.arrayOf(
-                PropTypes.shape({
-                    order: PropTypes.number,
-                    body: PropTypes.string
-                })
-            ),
-            id: PropTypes.string,
-            order: PropTypes.number,
-            prompt: PropTypes.string
-        })
-    ).isRequired,
+    questions: PropTypes.shape({
+        questions: PropTypes.arrayOf(
+            PropTypes.shape({
+                answers: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        order: PropTypes.number,
+                        body: PropTypes.string
+                    })
+                ),
+                id: PropTypes.string,
+                order: PropTypes.number,
+                prompt: PropTypes.string
+            })
+        ).isRequired,
+        loading: PropTypes.bool.isRequired,
+        error: PropTypes.bool.isRequired
+    }).isRequired,
     getQuestions: PropTypes.func.isRequired
 };
 
