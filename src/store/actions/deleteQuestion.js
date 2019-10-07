be8578b6442f68916541626c631fa31533264ab8
kboul <kos.voulgaridis@gmail.com>
2019-10-07 19:48:50 +0300

    Save deleted question to the DB

--- src/store/actions/deleteQuestion.js
+++ src/store/actions/deleteQuestion.js
@@ -1,3 +1,4 @@
+import httpService from '../../services/httpService';
 import { types } from './types';
 
 /**
@@ -6,8 +7,13 @@ import { types } from './types';
  */
 
 export const deleteQuestion = id => {
-    return async dispatch => {
+    return async (dispatch, getState) => {
         try {
+            const questions = [...getState().questions.questions];
+            const question = questions.find(q => q.id === id);
+            const index = questions.indexOf(question);
+            questions.splice(index, 1);
+            await httpService.post(`/api/questionnaire`, questions);
             dispatch({
                 type: types.DELETE_QUESTION,
                 id
