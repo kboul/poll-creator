2460cfd4927b40a6fd504c3e44e9501eb5c69dcd
kboul <kos.voulgaridis@gmail.com>
2019-10-07 17:30:02 +0300

    Add onTrashClick prop on Icons component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '../sass/Icons.module.sass';
 
-const Icons = () => {
+const Icons = ({ onTrashClick }) => {
     return (
         <div className="float-right">
             <i className={`fa fa-angle-up ${styles.faUp} ${styles.fa}`} />
             <i className={`fa fa-angle-down ${styles.faDown} ${styles.fa}`} />
-            <i className={`fa fa-trash ${styles.fa}`} />
+            <i className={`fa fa-trash ${styles.fa}`} onClick={onTrashClick} />
         </div>
     );
+};
+
+Icons.propTypes = {
+    onTrashClick: PropTypes.func.isRequired
 };
 
 export default Icons;
