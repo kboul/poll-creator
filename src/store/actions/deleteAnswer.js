e924579c68d9c308b513e39b2baaa39876d28778
kboul <kos.voulgaridis@gmail.com>
2019-10-07 20:02:28 +0300

    Save deleted answer to the DB

--- src/store/actions/deleteAnswer.js
+++ src/store/actions/deleteAnswer.js
@@ -1,3 +1,4 @@
+import httpService from '../../services/httpService';
 import { types } from './types';
 
 /**
@@ -7,8 +8,21 @@ import { types } from './types';
  */
 
 export const deleteAnswer = (id, order) => {
-    return async dispatch => {
+    return async (dispatch, getState) => {
         try {
+            const questions = [...getState().questions.questions];
+            const question = questions.find(q => q.id === id);
+            // exlude current answer
+            const remainingAnswers = question.answers.filter(
+                a => a.order !== order
+            );
+
+            const data = {
+                answers: [...remainingAnswers]
+            };
+
+            await httpService.put(`/api/questions/${id}`, data);
+
             dispatch({
                 type: types.DELETE_ANSWER,
                 id,
