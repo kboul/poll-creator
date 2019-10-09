d118970f7b18276165388fd183b229f80fd1d9b0
kboul <kos.voulgaridis@gmail.com>
2019-10-09 18:27:10 +0300

    Extend Icons to have a onAngleDownClick prop

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../sass/Icons.module.sass';
 
-const Icons = ({ onTrashClick }) => {
+const Icons = ({ onAngleDownClick, onTrashClick }) => {
     return (
         <div className="float-right">
             <i className={`fa fa-angle-up ${styles.faUp} ${styles.fa}`} />
-            <i className={`fa fa-angle-down ${styles.faDown} ${styles.fa}`} />
+            <i
+                className={`fa fa-angle-down ${styles.faDown} ${styles.fa}`}
+                onClick={onAngleDownClick}
+            />
             <i className={`fa fa-trash ${styles.fa}`} onClick={onTrashClick} />
         </div>
     );
 };
 
 Icons.propTypes = {
+    onAngleDownClick: PropTypes.func.isRequired,
     onTrashClick: PropTypes.func.isRequired
 };
 
