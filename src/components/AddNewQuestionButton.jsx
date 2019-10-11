58da00d0654e0c71dbd6dc73c936b84960aab881
kboul <kos.voulgaridis@gmail.com>
2019-10-11 12:15:29 +0300

    Change AddNewQuestionButton border radius

--- src/components/AddNewQuestionButton.jsx
+++ src/components/AddNewQuestionButton.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createQuestion } from '../store/actions/createQuestion';
 
+const borderRadius = { borderRadius: '20px' };
+
 const AddNewQuestionButton = ({ createQuestion }) => {
     return (
         <div className="offset-md-5">
             <button
                 type="button"
+                style={borderRadius}
                 className="btn btn-primary"
                 onClick={() => createQuestion()}>
                 Add new question
