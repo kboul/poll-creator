3f85843f41341e5148498723538cf2074c7380dd
kboul <kos.voulgaridis@gmail.com>
2019-10-14 22:12:11 +0300

    Implement reorder answer up without taking into account network errors

--- src/store/actions/reorderAnswerUp.js
+++ src/store/actions/reorderAnswerUp.js
@@ -1,10 +1,6 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable operator-assignment */
-/* eslint-disable func-names */
 /* eslint-disable no-param-reassign */
-/* eslint-disable no-return-assign */
 import { types } from './types';
-// import httpService from '../../services/httpService';
+import httpService from '../../services/httpService';
 
 /**
  *
@@ -17,6 +13,44 @@ export const reorderAnswerUp = (id, order) => {
         try {
             const questions = [...getState().questions.questions];
             const question = questions.find(q => q.id === id);
+            // find the answer to be changed
+            const answer = question.answers.find(a => a.order === order);
+            // change the answer to the new order by reducing one
+            const newAnswer = { ...answer, order: order - 1 };
+            // find the previousAnswer in order to chnage its order as well
+            const previousAnswer = question.answers.find(
+                a => a.order === order - 1
+            );
+
+            // take into account the answer with order number 1
+            if (previousAnswer) {
+                previousAnswer.order = order;
+
+                question.answers = [
+                    ...question.answers.filter(
+                        a => a.order !== order && a.order !== order - 1
+                    ),
+                    newAnswer,
+                    previousAnswer
+                ];
+            } else {
+                question.answers.forEach(a => {
+                    a.order -= 1;
+                });
+                answer.order = question.answers.length;
+
+                question.answers = [
+                    answer,
+                    ...question.answers.filter(
+                        a => a.order !== question.answers.length
+                    )
+                ];
+            }
+
+            // sort the array after the changes
+            question.answers.sort((a, b) => a.order - b.order);
+
+            await httpService.post('/api/questionnaire?debug=true', questions);
 
             dispatch({
                 type: types.REORDER_ANSWER_UP_SUCCESS
