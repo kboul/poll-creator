1397e5ebc068c954566b9cb5fe1bdfe4013e2d93
kboul <kos.voulgaridis@gmail.com>
2019-10-15 12:59:08 +0300

    Add comma to the alerts that do not have in the end of their sentence

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,45 +7,45 @@ const Alert = ({ type }) => {
 
     switch (type) {
         case 'fetchQuestions':
-            message += 'fetching the questions';
+            message += 'fetching the questions.';
             break;
         case 'createQuestion':
-            message += 'creating the question';
+            message += 'creating the question.';
             break;
         case 'tenQuestionsReached':
             message = 'You cannot add more than 10 questions.';
             alert = 'warning';
             break;
         case 'createAnswer':
-            message += 'creating the answer';
+            message += 'creating the answer.';
             break;
         case 'noQuestions':
             message = 'No questions have been created yet.';
             alert = 'warning';
             break;
         case 'deleteQuestion':
-            message += 'deleting the question';
+            message += 'deleting the question.';
             break;
         case 'deleteAnswer':
-            message += 'deleting the answer';
+            message += 'deleting the answer.';
             break;
         case 'reorderQuestionUp':
-            message += 'reordering the question up';
+            message += 'reordering the question up.';
             break;
         case 'reorderQuestionDown':
-            message += 'reordering the question down';
+            message += 'reordering the question down.';
             break;
         case 'updateQuestion':
-            message += 'updating the question';
+            message += 'updating the question.';
             break;
         case 'updateAnswer':
-            message += 'updating the answer';
+            message += 'updating the answer.';
             break;
         case 'reorderAnswernUp':
-            message += 'reordering the answer up';
+            message += 'reordering the answer up.';
             break;
         case 'reorderAnswernDown':
-            message += 'reordering the answer down';
+            message += 'reordering the answer down.';
             break;
         default:
             return;
